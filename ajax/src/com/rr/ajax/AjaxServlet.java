1931645e3b0da03537010969ae4e2f5bb67f472f
thinkerr24 <thinkerr24@163.com>
2018-08-13 21:37:05 +0800

    add ajax demo

---
+++ ajax/src/com/rr/ajax/AjaxServlet.java
@@ -0,0 +1,40 @@
+package com.rr.ajax;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxServlet
+ */
+@WebServlet("/ajaxServlet")
+public class AjaxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// response.getWriter().write("zhangsan");
+		String name = request.getParameter("name");
+/*		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		response.getWriter().write(Math.random()+name);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ajax/src/com/rr/ajax/AjaxServlet2.java
@@ -0,0 +1,39 @@
+package com.rr.ajax;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxServlet2
+ */
+@WebServlet("/ajaxServlet2")
+public class AjaxServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		System.out.print("name:"+name+", age:" + age);
+		String json = "{\"result\":\"³É¹¦!\", \"age\":25}";
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
